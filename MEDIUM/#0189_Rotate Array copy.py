6bcfef20484d5668a99da739f815b5232539d150
Mayuraksha Sikdar <mayurakshasikdar@gmail.com>
2025-04-04 13:58:55 +0100

    169. Majority Element

---
+++ MEDIUM/#0189_Rotate Array copy.py
@@ -0,0 +1,38 @@
+# 169. Majority Element
+
+
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than ⌊n / 2⌋ times.
+# You may assume that the majority element always exists in the array.
+
+# Example 1:
+# Input: nums = [3,2,3]
+# Output: 3
+
+# Example 2:
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+
+# Constraints:
+# n == nums.length
+# 1 <= n <= 5 * 104
+# -109 <= nums[i] <= 109
+
+# Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        elements = {}.fromkeys(list(set(nums)), 0)
+        __max = float('-inf')
+        for n in nums:
+            elements[n] += 1
+            if elements.get(__max, 0) < elements[n]:
+                __max = n
+        return __max
+        
+
+print(Solution().majorityElement(nums = [3,2,3]))
+print(Solution().majorityElement(nums = [2,2,1,1,1,2,2]))
